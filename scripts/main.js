73594641ad4d2fbf29744bb4ebf33c52f7ac12c6
Kchen1515 <99617496+Kchen1515@users.noreply.github.com>
2022-09-23 18:52:20 -0400

    Initial Commit

---
+++ scripts/main.js
@@ -0,0 +1,15 @@
+const myHeading = document.querySelector('h1')
+myHeading.textContent = 'Low Cost Therapy In NYC'
+myHeading.addEventListener('click',function () {
+    alert('Welcome to Thearpy NYC')
+})
+
+const myImage = document.querySelector('img')
+myImage.onclick = () => {
+    const mySrc = myImage.getAttribute('src')
+    if(mySrc === 'assets/therapy2.jpg'){
+        myImage.setAttribute('src','assets/therapy1.jpg')
+    }else{
+        myImage.setAttribute('src','assets/therapy2.jpg')
+    }
+}
