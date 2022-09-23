1299edf285d5f816379158044d0c6ed392d6ab82
KongYu Chen <kychen249@gmail.com>
2022-09-23 19:14:06 -0400

    Test commit on Mac

--- scripts/main.js
+++ scripts/main.js
@@ -1,7 +1,7 @@
 const myHeading = document.querySelector('h1')
 myHeading.textContent = 'Low Cost Therapy In NYC'
 myHeading.addEventListener('click',function () {
-    alert('Welcome to Thearpy NYC')
+    alert('Welcome to Thearpy NY')
 })
 
 const myImage = document.querySelector('img')
