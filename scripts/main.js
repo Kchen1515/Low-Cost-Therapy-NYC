8c04170edc3c44aba58a734c88427c406f95f8db
KongYu Chen <kychen249@gmail.com>
2022-09-23 22:32:47 -0400

    Day 1 End Commit

--- scripts/main.js
+++ scripts/main.js
@@ -1,15 +1,34 @@
 const myHeading = document.querySelector('h1')
 myHeading.textContent = 'Low Cost Therapy In NYC'
-myHeading.addEventListener('click',function () {
-    alert('Welcome to Thearpy NY')
-})
+// myHeading.addEventListener('click',function () {
+//     alert('Welcome to Therapy NY')
+// })
 
-const myImage = document.querySelector('img')
-myImage.onclick = () => {
-    const mySrc = myImage.getAttribute('src')
-    if(mySrc === 'assets/therapy2.jpg'){
-        myImage.setAttribute('src','assets/therapy1.jpg')
-    }else{
-        myImage.setAttribute('src','assets/therapy2.jpg')
-    }
-}
+// let myButton = document.querySelector('button')
+
+// function setUserName (){
+//     const myName = prompt('Please Enter Your Name')
+//     localStorage.setItem('name', myName )
+//     myHeading.textContent = `${myName}, Welcome to Therapy NYC`
+    
+// }
+// if(!localStorage.getItem('name')){
+//     setUserName()
+// }else{
+//     const storedName = localStorage.getItem('name')
+//     myHeading.textContent = `${storedName}, Welcome to Therapy NYC`
+// }
+
+// myButton.onclick = () =>{
+//     setUserName()
+// }
+
+// const myImage = document.querySelector('img')
+// myImage.onclick = () => {
+//     const mySrc = myImage.getAttribute('src')
+//     if(mySrc === 'assets/therapy2.jpg'){
+//         myImage.setAttribute('src','assets/therapy1.jpg')
+//     }else{
+//         myImage.setAttribute('src','assets/therapy2.jpg')
+//     }
+// }
